2e0a39670de03641bf0f6c356c6f5de37d96f946
ManavR123 <manav.rathod@berkeley.edu>
2020-07-19 00:30:23 -0400

    setup roslibjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { Joystick } from 'react-joystick-component';
 import { makeStyles } from '@material-ui/core/styles';
+import ROSLIB from 'roslib';
 
 const useStyles = makeStyles(() => ({
   controlPanel: {
@@ -10,13 +11,44 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+// ROS Setup
+const ros = new ROSLIB.Ros({
+  url: 'ws://168.61.20.118:9090',
+});
+
+const cmdDriveInfo = new ROSLIB.Topic({
+  ros: ros,
+  name: '/drive_info',
+  messageType: 'geometry_msgs/Vector3',
+});
+
 const App = () => {
   const classes = useStyles();
 
+  useEffect(() => {
+    // Setup ROS callbacks
+    ros.on('connection', function () {
+      console.log('Connected to websocket server.');
+    });
+
+    ros.on('error', function (error) {
+      console.log('Error connecting to websocket server: ', error);
+    });
+
+    ros.on('close', function () {
+      console.log('Connection to websocket server closed.');
+    });
+  }, []);
+
   const handleMove = (data) => {
-    const norm = Math.sqrt(data.x ** 2 + data.y ** 2);
+    const norm = 50; // Max absolute value of coordinates
     const x = data.x / norm;
     const y = data.y / norm;
+    const drive_msg = new ROSLIB.Message({
+      x: x,
+      y: y,
+    });
+    cmdDriveInfo.publish(drive_msg);
   };
 
   return (
